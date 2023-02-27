bd8ae24a219de5df03e517321049ea06a243f4a1
mahdi <mahdimotallebe@gmail.com>
2023-02-27 17:07:19 +0330

    Change Font

--- styles/global/globals.ts
+++ styles/global/globals.ts
@@ -16,7 +16,7 @@ export const GlobalStyle = createGlobalStyle`
           font-size: 62.5%;
           max-width: 1800px;
           overflow-x: hidden;         
-          font-family: 'Roboto', 'sans-serif';
+          font-family: 'nunito', 'sans-serif';
           
           
       }
